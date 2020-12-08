ba82d8d16fecdcac3e2cf02784581feb58067af8
Raphael Villas <raphael.villas@gmail.com>
2020-12-07 22:18:56 -0600

    Committed hello world file

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi there!");
+            Console.WriteLine("Welcome to C# and Unity!");
+        }
+    }
+}
